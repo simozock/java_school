2f2675e5acdc7ab1214904ef22093acb669191dc
Simone Zoccola <zoccola.simone2001@gmail.com>
2023-04-30 15:05:22 +0200

    First Hello

---
+++ JavaGitSchool/src/Prove/HelloGit.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Prove;
+
+/**
+ *
+ * @author simone
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
